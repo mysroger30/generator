73c094ea218161db92a2deca7ce9eafb11224b8e
Dan Setterquist <dansetterquist@gmail.com>
2016-01-25 09:15:13 +0100

    Improved generate script.

--- generate.sh
+++ generate.sh
@@ -1,41 +1,46 @@
-if [[ $# -lt 2 ]] ; then
-    echo 'generate [project] [output]'
+if [[ $# -lt 1 ]] ; then
+    echo 'generate [project]'
     exit 0
 fi
 
-if [ -d "tmp" ]; then
-    rm -r "tmp"
+tmp_dir="tmp"
+project_name="$1"
+out_dir="$project_name-out"
+out_dir_questions="fragor"
+
+if [ -d "$tmp_dir" ]; then
+    rm -r "$tmp_dir"
 fi
-mkdir "tmp"
+mkdir "$tmp_dir"
 
-jq -f sanitize.jq "data/$1/database.json" | jq -f process.jq > tmp/processed.json
+jq -f sanitize.jq "data/$project_name/database.json" | jq -f process.jq > "$tmp_dir/processed.json"
 
-numq=$(($(jq "length" tmp/processed.json) - 1))
+numq=$(($(jq "length" "$tmp_dir/processed.json") - 1))
 
-seq 0 $numq | parallel jq '.[{}]' tmp/processed.json '>' tmp/{}
+seq 0 $numq | parallel jq '.[{}]' "$tmp_dir/processed.json" '>' "$tmp_dir/{}"
 
-seq 0 $numq | parallel mustache tmp/{} template/question.mustache '>' tmp/{}.html
+seq 0 $numq | parallel mustache "$tmp_dir/{}" template/question.mustache '>' "$tmp_dir/{}.html"
 
-if [ -d "$2" ]; then
-    rm -r "$2"
+if [ -d "$out_dir" ]; then
+    rm -r "$out_dir"
 fi
-mkdir "$2"
-mkdir "$2/fragor"
+mkdir "$out_dir"
+mkdir "$out_dir/$out_dir_questions"
 
 for i in $(seq 0 $numq)
 do
-    file_name="$(cat "tmp/$i" | jq -r ".stem" | sed -e 's/\(.*\)/\L\1/' | sed 'y/åäö /aao-/' | sed "s/[^[:alpha:]-]/-/g")"
+    file_name="$(cat "$tmp_dir/$i" | jq -r ".stem" | sed -e 's/\(.*\)/\L\1/' | sed 'y/åäö /aao-/' | sed "s/[^[:alpha:]-]/-/g")"
     file_name="$(echo $file_name | sed 's/--*/-/g' | cut -c 1-50).html"
     
-    mkdir "$2/fragor/$i"
-    cp "tmp/$i.html" "$2/fragor/$i/$file_name"
+    mkdir "$out_dir/$out_dir_questions/$i"
+    cp "$tmp_dir/$i.html" "$out_dir/$out_dir_questions/$i/$file_name"
 done
 
-cp -r "data/$1/images" "$2/"
+cp -r "data/$project_name/images" "$out_dir/"
 
-cp -r "template/css" "$2/"
+cp -r "template/css" "$out_dir/"
 
-mkdir "$2/js"
-cat "template/js/index.js" "data/$1/database.json" > "$2/js/index.js"
+mkdir "$out_dir/js"
+cat "template/js/index.js" "data/$project_name/database.json" > "$out_dir/js/index.js"
 
-cp "template/index.mustache" "$2/index.html"
+cp "template/index.mustache" "$out_dir/index.html"
